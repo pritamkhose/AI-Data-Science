943265be29158d69015ed9a63f98fdae7fcd3e9f
Pritam <pritamkhose@gmail.com>
2020-08-08 19:58:08 +0530

    Update chart.py

--- Stock/bseindia/chart.py
+++ Stock/bseindia/chart.py
@@ -20,21 +20,30 @@ import matplotlib.pyplot as plt
 import json
 import requests
 # icici 532174  infy 500209
-url = 'https://charting.bseindia.com/charting/RestDataProvider.svc/getDat?exch=N&scode=500209&type=b&mode=bseL&fromdate=01-01-1991-01:01:00-AM'
-datajson = json.loads(requests.post(url).json()['getDatResult'])
-datainval = datajson['DataInputValues'][0]
+
+headers = {'User-Agent': 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'}
+
+#url =  'https://charting.bseindia.com/charting/RestDataProvider.svc/getDatI?exch=N&scode=532174&type=b&mode=bseL&fromdate=01-01-1991-01:01:00-AM'
+#url =  'https://charting.bseindia.com/charting/RestDataProvider.svc/getDatI?exch=N&scode=16&type=i&mode=bseL&fromdate=01-01-1991-01:01:00-AM'
+url =  'https://charting.bseindia.com/charting/RestDataProvider.svc/getDat?exch=N&scode=532174&type=b&mode=bseL&fromdate=01-01-1991-01:01:00-AM' #Daily
+#url =  'https://charting.bseindia.com/charting/RestDataProvider.svc/getDat?exch=N&scode=532174&type=b&mode=bseL&fromdate=07-08-2020-12:00:00-AM' #week
+#url =  'https://charting.bseindia.com/charting/RestDataProvider.svc/getDat?exch=N&scode=532174&type=b&mode=bseL&fromdate=07-08-2020-12:00:00-AM' #year
+
+datainval = requests.post(url, headers=headers).json()['getDatResult'] #getDatIResult
+datainval =(json.loads(datainval))['DataInputValues'][0]
 
 OpenData = datainval['OpenData'][0]['Open'].split(",")
 CloseData = datainval['CloseData'][0]['Close'].split(",")
 LowValues = datainval['LowData'][0]['Low'].split(",")
+HighData = datainval['HighData'][0]['High'].split(",")
 VolumeData = datainval['VolumeData'][0]['Volume'].split(",")
 DateData = datainval['DateData'][0]['Date'].split(",")
 
 PVList = [];
 
 for row in range(len(OpenData)):
-    PVList.append([row, pd.to_datetime(DateData[row]), float(OpenData[row]) , float(CloseData[row]), float(LowValues[row]), float(VolumeData[row])])
-df = pd.DataFrame(PVList, columns =['Index','Date', 'Open', 'Close', 'Low', 'Volume']) 
+    PVList.append([row, pd.to_datetime(DateData[row]), float(OpenData[row]) , float(CloseData[row]), float(HighData[row]), float(LowValues[row]), float(VolumeData[row])])
+df = pd.DataFrame(PVList, columns =['Index', 'Date', 'Open', 'Close', 'High', 'Low', 'Volume']) 
 
 vper =  10000 #df['Volume'].mean()*0.1
 #Visualize the data
