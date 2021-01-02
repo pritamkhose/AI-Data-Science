31a2b17eaa633492247805c5722a6c5777f84bf0
Pritam Khose <pritamkhose@gmail.com>
2021-01-02 22:58:49 +0530

    Update calculateprice.py

--- Stock/moneycontrol/calculateprice.py
+++ Stock/moneycontrol/calculateprice.py
@@ -6,96 +6,147 @@ Created on Sun Mar 29 11:27:22 2020
 
 """
 
-#Import the libraries
+# Import the libraries
 import os
 import math
+from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-#plt.style.use('fivethirtyeight')
+
 import json
 import requests
 
-#1d 5d max
-compname = 'IT'
-#url = 'https://www.moneycontrol.com/mc/widget/basicchart/get_chart_value?classic=true&dur=max&sc_did=' + compname
-#datajson = requests.get(url).json()
-#
-#if not os.path.exists('data'):
-#    os.makedirs('data')
-## Writing to json file
-#with open('data/'+compname + '.json', 'w') as outfile: 
-#    outfile.write(json.dumps(datajson))
+
+compname = 'IT' #SBI TCS W HDF01 SBI HDF01
+
+# Get Data #1d 5d max
+url = 'https://www.moneycontrol.com/mc/widget/basicchart/get_chart_value?classic=true&dur=max&sc_did=' + compname
+datajson = requests.get(url).json()
+
+if not os.path.exists('data'):
+    os.makedirs('data')
+# Writing to json file
+with open('data/'+compname + '.json', 'w') as outfile:
+    outfile.write(json.dumps(datajson))
 
 # Read to json file
 with open('data/'+compname + '.json') as json_file:
     datajson = json.load(json_file)
-
-#newsdata = datajson['newsdata']
 stock = datajson['g1']
-PVList = [];
+
+PVList = []
 for row in range(len(stock)):
-    PVList.append([pd.to_datetime(stock[row]['date']), stock[row]['date'], float(stock[row]['value']), 
-                   float(stock[row]['open']) , float(stock[row]['close']), 
-                   float(stock[row]['low']), float(stock[row]['high']), 
+    PVList.append([pd.to_datetime(stock[row]['date']), stock[row]['date'], float(stock[row]['value']),
+                   float(stock[row]['open']), float(stock[row]['close']),
+                   float(stock[row]['low']), float(stock[row]['high']),
                    float(stock[row]['volume'])
                    ])
-df = pd.DataFrame(PVList ,columns =['date', 'datestr','value', 'open', 'close', 'low', 'high', 'volume']) 
+df = pd.DataFrame(PVList, columns=[
+                  'date', 'datestr', 'value', 'open', 'close', 'low', 'high', 'volume'])
 
 
-bdsList = [];
-bonus = datajson['data']['bonus']
-for row in range(len(bonus)):
-    arr = (bonus[row]['ratio']).split(':')
-    bdsList.append(['bonus', bonus[row]['date'].replace(', ','-'), arr[0], arr[1]])
-    
-dividends = datajson['data']['dividends']
-for row in range(len(dividends)):
-    arr = (dividends[row]['ratio']).split(' ')
-    bdsList.append(['dividends', dividends[row]['date'].replace(', ','-'), arr[0], arr[1]])
-    
-splits = datajson['data']['splits']
-for row in range(len(splits)):
-    arr = (splits[row]['ratio']).split('-')
-    bdsList.append(['splits',splits[row]['date'].replace(', ','-'), arr[0], arr[1]])
-bdsdf = pd.DataFrame(bdsList ,columns =['type', 'date', 'para1', 'para2']) 
+bdsList = []
+
+try:
+    bonus = datajson['data']['bonus']
+    for row in range(len(bonus)):
+        arr = (bonus[row]['ratio']).split(':')
+        date = bonus[row]['date'].replace(', ', '-')
+        bdsList.append(['bonus', date, str(date), arr[0], arr[1]])
+except:
+    print("No bonus!")
+
+try:
+    dividends = datajson['data']['dividends']
+    for row in range(len(dividends)):
+        arr = (dividends[row]['ratio']).split(' ')
+        date = dividends[row]['date'].replace(', ', '-')
+        bdsList.append(['dividends', date, str(date), arr[0], arr[1]])
+except:
+    print("No dividends!")
+
+try:
+    splits = datajson['data']['splits']
+    for row in range(len(splits)):
+        arr = (splits[row]['ratio']).split('-')
+        date = splits[row]['date'].replace(', ', '-')
+        bdsList.append(['splits', date, str(date), arr[0], arr[1]])
+except:
+    print("No splits!")
+
+
+try:
+    rights = datajson['data']['rights']
+    for row in range(len(rights)):
+        arr = (rights[row]['ratio']).split(':')
+        date = rights[row]['date'].replace(', ', '-')
+        bdsList.append(['rights', date, str(date), arr[0], arr[1]])
+except:
+    print("No rights!")
+
+bdsdf = pd.DataFrame(
+    bdsList, columns=['type', 'date', 'datestr', 'para1', 'para2'])
 bdsdf = bdsdf.sort_values(by='date')
-#bdsdf.index = bdsdf['date']
+bdsdf.reset_index(inplace=True)
+
 
 stockunit = 10
-PVList = [];
-bdsList = bdsdf.index.values.tolist()
-for row in range(len(bdsList)):
-    #if(bdsdf['date'][row] == stock[row]['date']) :
-    print(bdsdf.iloc[[row]])
-#    if(bdsdf['type'][row] == 'bonus') :
-#        print(bdsdf.iloc[[row]])
-#    elif(bdsdf['type'][row] == 'dividends') :
-#        print(bdsdf.iloc[[row]])  
-#    elif(bdsdf['type'][row] == 'splits') :
-#        print(bdsdf.iloc[[row]])
-#    else :
-#        print(bdsdf.iloc[[row]])
-        
-#    netprice = stockunit * float(stock[row]['value'])
-#    PVList.append([stockunit, netprice])
-#df = pd.DataFrame(PVList ,columns =['date', 'value', 'open', 'close', 'low', 'high', 'volume', 'stockunit', 'netprice']) 
+startUnit = stockunit
+facevalue = 10
+aList = []
+for row in range(len(bdsdf)):
+    currentDate = str(bdsdf['datestr'][row])
+
+    findRow = df.loc[df['datestr'].isin([currentDate])]
+
+    resultrow = []
+    if(len(findRow) > 0):
+        i = findRow.index[0]
+        resultrow = [currentDate, findRow['date'][i], findRow['value'][i]]
+    else:
+        datetime_object = datetime.strptime(currentDate, '%Y-%M-%d')
+        datearr = [(datetime_object - timedelta(days=1)).strftime("%Y-%M-%d"),
+                   (datetime_object - timedelta(days=2)).strftime("%Y-%M-%d"),
+                   (datetime_object - timedelta(days=3)).strftime("%Y-%M-%d")]
+        findRow = df.loc[df['datestr'].isin(datearr)]
+        i = findRow.index[(len(findRow)-1)]
+        resultrow = [currentDate, findRow['date'][i], findRow['value'][i]]
+
+    dividend = 0
+    if(bdsdf['type'][row] == 'splits'):
+        stockunit = stockunit * \
+            (int(bdsdf['para1'][row]) / int(bdsdf['para2'][row]))
+        facevalue = facevalue / \
+            (int(bdsdf['para1'][row]) / int(bdsdf['para2'][row]))
+    elif (bdsdf['type'][row] == 'bonus'):
+        stockunit = stockunit + \
+            (stockunit * (int(bdsdf['para1'][row]) / int(bdsdf['para2'][row])))
+    elif (bdsdf['type'][row] == 'dividends'):
+        dividend = (float(bdsdf['para2'][row]) / 100) * stockunit
+    elif (bdsdf['type'][row] == 'rights'):
+        stockunit = stockunit + stockunit / \
+            (int(bdsdf['para2'][row]) / int(bdsdf['para1'][row]))
+
+    aList.append([resultrow[0], resultrow[1], resultrow[2],
+                  stockunit, facevalue, dividend])
 
 
+dfcal = pd.DataFrame(aList, columns=[
+                     'currentDate', 'finddate', 'unitprice', 'stockunit', 'facevalue', 'dividend'])
+col = ['finddate', 'unitprice', 'stockunit', 'facevalue', 'dividend']
+bdsdf[col] = dfcal[col]
 
-
-# df.index = df['date']
-
-
-#vper =  1000000 #df['Volume'].mean()*0.1
-##Visualize the data
-#plt.figure(figsize=(16,8))
-#plt.title('Stock ')
-#plt.xlabel('Date', fontsize=10)
-#plt.ylabel('Price', fontsize=10)
-#plt.plot(df['value'], label = "Price", linewidth=1,)
-#plt.plot((df['volume']/vper), label = "Volume", linewidth=1,)
-#plt.legend()
-#plt.grid()
-#plt.savefig("1.png")
-#plt.show()    
+totalDividend = dfcal['dividend'].sum()
+print('Total Divident = ₹ ' + str(totalDividend))
+noUnit = bdsdf['stockunit'][len(bdsdf)-1]
+print('No. of Stocks = ' + str(noUnit) + ' No.s')
+print('Invest Date = ' + str(df['date'][0]))
+investPrice = df['value'][0]
+print('Invest Rate = ₹ ' + str(investPrice))
+print('Invest Price = ' + str(startUnit * investPrice))
+todayPrice = df['value'][len(df)-1]
+print('Current Rate = ' + str(todayPrice))
+print('Current Price = ₹ ' + str(noUnit * todayPrice))
+print('Retrun = ' + str((noUnit * todayPrice)/(startUnit * investPrice)) + ' times')
+print('Retrun with Divident = ' + str((totalDividend +
+                                       (noUnit * todayPrice))/(startUnit * investPrice)) + ' times')
