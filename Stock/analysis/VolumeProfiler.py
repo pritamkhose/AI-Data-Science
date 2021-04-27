f621ad0eb86ccbb6437970c9f3ef39b031b73c25
Pritam Khose <pritamkhose@gmail.com>
2021-04-27 23:31:11 +0530

    Update VolumeProfiler.py

--- Stock/analysis/VolumeProfiler.py
+++ Stock/analysis/VolumeProfiler.py
@@ -64,29 +64,32 @@ def volumeYearRange(days):
     return dfochl
 
 
-dfochl = volumeYearRange(300)
+# dfochl = volumeYearRange(300)
 
 
 def volumePriceRange(fpath, step, days):
     df = pd.DataFrame(json.load(open(fpath))['g1'])
-    df[['close', 'volume']] = df[['close', 'volume']].astype(float)
-    df = df[['date', 'close', 'volume']]
+    df[['high', 'low', 'open', 'close', 'volume']] = df[[
+        'high', 'low', 'open', 'close', 'volume']].astype(float)
 
     df['volumeEMA20'] = df['volume'].ewm(span=20, adjust=False).mean()
-    df['volumeEMA20'] = df['volume']/df['volumeEMA20']
+    # df['volumeEMA20'] = df['volume']/df['volumeEMA20']
 
     dflen = len(df)
     if(dflen-days) > 0:
         df = df[dflen-days:dflen]
     else:
         df
-    dfmax = df.close.max()
+    dfmax = df.high.max()
     dfper = (dfmax/100)
     dfmax = dfmax/dfper
-    dfmin = df.close.min()/dfper
+    dfmin = df.low.min()/dfper
     dfrange = (dfmax - dfmin) / step
 
     df['closePer'] = df.close/dfper
+    df['openPer'] = df.open/dfper
+    df['lowPer'] = df.low/dfper
+    df['highPer'] = df.high/dfper
 
     rangeList = [dfmin]
     for i in range(step):
@@ -94,10 +97,26 @@ def volumePriceRange(fpath, step, days):
 
     aList = []
     for row in range(len(rangeList)-1):
-        aList.append([row, len(df[df['closePer'].between(rangeList[row], rangeList[row+1])]),
-                      round((dfper * rangeList[row]), 2), round((dfper * rangeList[row+1]), 2)])
-    return aList
+        closePer = (df[df['closePer'].between(
+            rangeList[row], rangeList[row+1])])
+        closeVolume = closePer['volume'].sum()
+        closeEMAVolume = closePer['volumeEMA20'].sum()
+        openPer = (df[df['openPer'].between(rangeList[row], rangeList[row+1])])
+        lowPer = (df[df['lowPer'].between(rangeList[row], rangeList[row+1])])
+        highPer = (df[df['highPer'].between(rangeList[row], rangeList[row+1])])
+        avg = (len(closePer) + len(openPer) + len(lowPer) + len(highPer))/4
+        aList.append([row, round((dfper * rangeList[row]), 2), round((dfper * rangeList[row+1]), 2),
+                      len(closePer), len(openPer), len(lowPer), len(highPer), avg, closeVolume, closeEMAVolume])
+
+    resultdf = pd.DataFrame(aList, columns=['row', 'priceRangeLow', 'priceRangeHigh',
+                                            'close', 'open', 'low', 'high',  'avg', 'closeVolSum', 'closeEMAVolume'])
+    return resultdf
 
 
 fpath = 'E:\Code\python\AIDataScience\Stock\moneycontrol\data\IT.json'
-alist = volumePriceRange(fpath, 20, 90)
+df = volumePriceRange(fpath, 20, 90)
+closeVolSum = df['closeVolSum'].sum() / 100
+closeEMAVolume = df['closeEMAVolume'].sum() / 100
+df['closeVolSumPer'] = df['closeVolSum'] / closeVolSum
+df['closeEMAVolumePer'] = df['closeEMAVolume'] / closeEMAVolume
+
